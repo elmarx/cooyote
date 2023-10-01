680e5be736d256703d6efd2bcc0183410d5b26a2
Elmar Athmer <elmar@athmer.org>
2023-10-01 15:19:24 +0200

    write sensor-data into a prometheus gauge

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,32 @@
+use std::error::Error;
 use std::thread;
 use std::time::Duration;
 
-fn main() {
-    let sensor = co2mon::Sensor::open_default().expect("error opening sensor");
+use prometheus::{Encoder, Gauge, Opts, Registry, TextEncoder};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let sensor = co2mon::Sensor::open_default()?;
+
+    let co2_gauge = Gauge::with_opts(Opts::new("co2_ppm", "co2 ppm"))?;
+    let temperature_gauge = Gauge::with_opts(Opts::new("temperature_celsius", "temperature in celsius"))?;
+
+    let r = Registry::new();
+    r.register(Box::new(co2_gauge.clone())).unwrap();
+    r.register(Box::new(temperature_gauge.clone())).unwrap();
 
     loop {
         match sensor.read() {
-            Ok(reading) => println!("{}°C {}ppm", reading.temperature(), reading.co2()),
+            Ok(reading) => {
+                co2_gauge.set(reading.co2() as f64);
+                temperature_gauge.set(reading.temperature() as f64);
+
+                let mut buffer = vec![];
+                let encoder = TextEncoder::new();
+                let metric_families = r.gather();
+                encoder.encode(&metric_families, &mut buffer).unwrap();
+
+                println!("{}", String::from_utf8(buffer).unwrap());
+            },
             Err(e) => eprintln!("{}", e),
         }
         thread::sleep(Duration::from_secs(60));
