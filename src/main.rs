05216675a9b2c73f8c440da5a953718797596d99
Elmar Athmer <elmar@athmer.org>
2023-10-01 16:45:56 +0200

    export metrics via http

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,45 @@
+use axum::routing::get;
+use axum::{Router, Server};
 use std::error::Error;
+use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
 
-use prometheus::{Encoder, Gauge, Opts, Registry, TextEncoder};
+use crate::metrics::metrics;
+use prometheus::{Gauge, Opts, Registry};
 
-fn main() -> Result<(), Box<dyn Error>> {
+mod metrics;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     let sensor = co2mon::Sensor::open_default()?;
 
     let co2_gauge = Gauge::with_opts(Opts::new("co2_ppm", "co2 ppm"))?;
-    let temperature_gauge = Gauge::with_opts(Opts::new("temperature_celsius", "temperature in celsius"))?;
+    let temperature_gauge =
+        Gauge::with_opts(Opts::new("temperature_celsius", "temperature in celsius"))?;
 
     let r = Registry::new();
     r.register(Box::new(co2_gauge.clone())).unwrap();
     r.register(Box::new(temperature_gauge.clone())).unwrap();
 
-    loop {
+    tokio::task::spawn_blocking(move || loop {
         match sensor.read() {
             Ok(reading) => {
                 co2_gauge.set(reading.co2() as f64);
                 temperature_gauge.set(reading.temperature() as f64);
-
-                let mut buffer = vec![];
-                let encoder = TextEncoder::new();
-                let metric_families = r.gather();
-                encoder.encode(&metric_families, &mut buffer).unwrap();
-
-                println!("{}", String::from_utf8(buffer).unwrap());
-            },
+            }
             Err(e) => eprintln!("{}", e),
         }
         thread::sleep(Duration::from_secs(60));
-    }
+    });
+
+    let app = Router::new()
+        .route("/metrics", get(metrics))
+        .with_state(Arc::new(r));
+
+    Server::bind(&"0.0.0.0:8080".parse()?)
+        .serve(app.into_make_service())
+        .await?;
+
+    Ok(())
 }
---
+++ src/metrics.rs
@@ -0,0 +1,18 @@
+use axum::extract::State;
+use axum::{debug_handler, headers::ContentType, TypedHeader};
+use prometheus::{Encoder, Registry, TextEncoder};
+use std::str::FromStr;
+use std::sync::Arc;
+
+#[debug_handler]
+pub async fn metrics(State(registry): State<Arc<Registry>>) -> (TypedHeader<ContentType>, Vec<u8>) {
+    let mut buffer = vec![];
+    let encoder = TextEncoder::new();
+    let metric_families = registry.gather();
+    encoder.encode(&metric_families, &mut buffer).unwrap();
+
+    (
+        TypedHeader(ContentType::from_str(encoder.format_type()).unwrap()),
+        buffer,
+    )
+}
